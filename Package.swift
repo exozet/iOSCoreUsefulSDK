d2093102bc72d3fe59e4a182a86503d8d6847610
burak.uzunboy <b_uzunboy@outlook.com>
2020-02-09 12:17:36 +0100

    Renaming

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "iOSCoreUsefulSDK",
+    name: "CoreUsefulSDK",
     platforms: [.iOS(.v10), .tvOS(.v10), .watchOS(.v2)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
