12192d7230808dfb9c0ec7c186ec79fefeda431f
burak.uzunboy <b_uzunboy@outlook.com>
2020-07-15 13:17:21 +0200

    Addition of raw representable user defaults property wrapper

--- Sources/CoreUsefulSDK/Structs/UserDefaultsWrapper.swift
+++ Sources/CoreUsefulSDK/Structs/UserDefaultsWrapper.swift
@@ -15,7 +15,7 @@ public struct UserDefaultsValue<Value> {
     /// The return value for the property if value cannot be identified by given key in `UserDefaults`.
     public let defaultValue: Value
     /// `UserDefaults` database. Default is `standard`.
-    public var userDefaults: UserDefaults = .standard
+    public var userDefaults: UserDefaults
 
     /// Returns wrapped value or sets to `UserDefaults`.
     public var wrappedValue: Value {
@@ -43,12 +43,38 @@ public struct UserDefaultsOptionalValue<Value> {
     /// The name of one of the receiver's properties.
     public let key: String
     /// `UserDefaults` database. Default is `standard`.
-    public var userDefaults: UserDefaults = .standard
+    public var userDefaults: UserDefaults
 
     /// Returns wrapped value or sets to `UserDefaults`.
     public var wrappedValue: Value? {
         get { userDefaults.value(forKey: key) as? Value }
         set { userDefaults.setValue(newValue, forKey: key) }
+    }
+    
+    /// Initializes Wrapper struct.
+    /// - Parameters:
+    ///   - key: The name of one of the receiver's properties.
+    ///   - defaultValue: The return value for the property if value cannot be identified by given key in `UserDefaults`.
+    ///   - userDefaults: `UserDefaults` database. Default is `standard`.
+    public init(key: String,
+                userDefaults: UserDefaults = .standard) {
+        self.key = key
+        self.userDefaults = userDefaults
+    }
+}
+
+/// Saves given type of optional value to the `UserDefaults`.
+@propertyWrapper
+public struct UserDefaultsRawRepresentableValue<Value: RawRepresentable> {
+    /// The name of one of the receiver's properties.
+    public let key: String
+    /// `UserDefaults` database. Default is `standard`.
+    public var userDefaults: UserDefaults
+
+    /// Returns wrapped value or sets to `UserDefaults`.
+    public var wrappedValue: Value? {
+        get { (userDefaults.value(forKey: key) as? Value.RawValue).flatMap({ Value(rawValue: $0) }) }
+        set { userDefaults.setValue(newValue?.rawValue, forKey: key) }
     }
     
     /// Initializes Wrapper struct.
