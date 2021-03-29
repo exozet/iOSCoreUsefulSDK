e8666c55f9f6732cb636a8108a0735434744b51d
Gerald Collaku <gerald.collaku@exozet.com>
2021-03-29 22:16:48 +0200

    Add the option to get the english localized string from the extension

--- Sources/CoreUsefulSDK/Extensions/String+Attributed.swift
+++ Sources/CoreUsefulSDK/Extensions/String+Attributed.swift
@@ -59,13 +59,21 @@ public extension String {
     /// Returns localized string.
     var localized: String {
         if NSLocalizedString(self, comment: self).isEmpty {
-             if let enPath = Bundle.main.path(forResource: "en", ofType: "lproj") {
-                let enBundle = Bundle(path: enPath)
-                return enBundle?.localizedString(forKey: self, value: self, table: nil) ?? ""
-            }
+          return enLocalized
         }
-        return  NSLocalizedString(self, comment: self)
+        return  NSLocalizedString(self, comment: self)
     }
+    
+    /// returns localized string in english or empty string
+    var enLocalized: String {
+        if let enPath = Bundle.main.path(forResource: "en", ofType: "lproj") {
+            let enBundle = Bundle(path: enPath)
+            return enBundle?.localizedString(forKey: self, value: self, table: nil) ?? ""
+        }
+        return ""
+    }
+    
+    
     
     /// Returns attributed string
     func attributed(with attributes: [NSAttributedString.Key:Any]) -> NSAttributedString {
