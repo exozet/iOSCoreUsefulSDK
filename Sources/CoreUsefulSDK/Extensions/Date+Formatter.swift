8557ea8f1bae6fc1d489bc8ccd361c054b9756ef
Philipp Homann <Philipp.Homann@exozet.com>
2021-04-16 17:34:11 +0200

    added some new date extensions

--- Sources/CoreUsefulSDK/Extensions/Date+Formatter.swift
+++ Sources/CoreUsefulSDK/Extensions/Date+Formatter.swift
@@ -23,6 +23,77 @@
 import Foundation
 
 public extension Date {
+
+    
+    var calendarIso8601: Calendar {
+        return Calendar(identifier: .iso8601)
+    }
+    
+    var dayOfMonth: Int {
+        return calendarIso8601.component(.day, from: self)
+    }
+    
+    var monthOfYear: String {
+        return month
+    }
+    
+    var year: Int {
+        return calendarIso8601.component(.year, from: self)
+    }
+    
+    var monthDays: [Date] {
+        
+        let monthDayRange = calendarIso8601.range(of: .day, in: .month, for: self)!
+        let days = (monthDayRange.lowerBound ..< monthDayRange.upperBound)
+            .compactMap { calendarIso8601.date(byAdding: .day, value: $0 - self.dayOfMonth, to: self) }
+        return days
+    }
+    
+    var startOfDay: Date {
+          return Calendar.current.startOfDay(for: self)
+      }
+    
+    var firstDayOfMonth: Date? {
+        return monthDays.first?.startOfDay
+    }
+    
+    var startOfMonth: Date {
+        let components = Calendar.current.dateComponents([.year, .month], from: self)
+        return  calendarIso8601.date(from: components)!
+    }
+
+    var endOfMonth: Date {
+        var components = DateComponents()
+        components.month = 1
+        components.second = -1
+        return Calendar.current.date(byAdding: components, to: startOfMonth)!
+    }
+    
+    
+    /// Returns ISO 8601 formated string of the date
+    func iso8601() -> String {
+        let formatter = DateFormatter()
+        let enUSPosixLocale = Locale(identifier: "en_US_POSIX")
+        formatter.locale = enUSPosixLocale
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        formatter.calendar = Calendar(identifier: .gregorian)
+        
+        return formatter.string(from: self)
+    }
+    
+    
+    /// returns an array of date object between two dates
+    static func dates(from fromDate: Date, to toDate: Date) -> [Date] {
+        var dates: [Date] = []
+        var date = fromDate
+        
+        while date <= toDate {
+            dates.append(date)
+            guard let newDate = Calendar.current.date(byAdding: .day, value: 1, to: date) else { break }
+            date = newDate
+        }
+        return dates
+    }
     
     /// Checks two date objects and compares do they represent same day on the calendar.
     /// - parameter date: Date object to compare
@@ -38,6 +109,13 @@ public extension Date {
         }
         
         return false
+    }
+    
+    /// Checks two date objects and compares do they represent same month on the calendar.
+    /// - parameter date: Date object to compare
+    /// - returns: `true` if two date objects have same month
+    func isSameMonth(as date: Date) -> Bool {
+        isEqual(to: date, toGranularity: .month)
     }
     
     /// Returns date object with the given format as string.
@@ -65,6 +143,17 @@ public extension Date {
     /// Returns `true` if the date corresponds to day after tomorrow.
     var isTheDayAfterTomorrow: Bool {
         return self.isSameDay(with: Calendar.current.date(byAdding: .day, value: +2, to: Date())!)
+    }
+    
+    
+    // MARK: private stuff
+    
+    private var month: String {
+        return self.format(with: "MMMM")
+    }
+    
+    private func isEqual(to date: Date, toGranularity component: Calendar.Component, in calendar: Calendar = .current) -> Bool {
+        calendar.isDate(self, equalTo: date, toGranularity: component)
     }
     
 }
